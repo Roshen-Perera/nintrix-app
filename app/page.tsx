4789af5b4e3342d8fcb5e411514576c297455ca7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-11 00:49:33 +0530

    fix: add paragraph to Home component for improved content display

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ratione facilis quas, incidunt numquam sint a quo cumque voluptatibus in cupiditate possimus dignissimos vitae recusandae? Est animi nobis cum illum!</p>
     </div>
   )
 }
