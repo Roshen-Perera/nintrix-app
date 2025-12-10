40de6fcd74fe481efefe1a882bb5070075b92a9c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-11 00:53:53 +0530

    fix: add styling classes to Home heading for improved appearance

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2 className='text-2xl font-semibold'>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ratione facilis quas, incidunt numquam sint a quo cumque voluptatibus in cupiditate possimus dignissimos vitae recusandae? Est animi nobis cum illum!</p>
     </div>
   )
