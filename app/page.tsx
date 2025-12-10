da0fd85aead364e9d88985e44168d47b823b1fc4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-11 00:55:00 +0530

    fix: add padding to Home component for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='p-10'>
       <h2 className='text-2xl font-semibold'>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ratione facilis quas, incidunt numquam sint a quo cumque voluptatibus in cupiditate possimus dignissimos vitae recusandae? Est animi nobis cum illum!</p>
     </div>
