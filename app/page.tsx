76d8dfef3c7400514a474f7e2887f1a0658fde49
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 21:07:01 +0530

    fix: replace div with Container component for improved layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+import Container from '@/components/Container'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <Container>
       <h2 className='text-2xl font-semibold'>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ratione facilis quas, incidunt numquam sint a quo cumque voluptatibus in cupiditate possimus dignissimos vitae recusandae? Est animi nobis cum illum!</p>
       <Button variant="default">Default Button</Button>
-    </div>
+    </Container>
   )
 }
 
