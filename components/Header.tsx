39a556fa383b44481e0f2bd4bc995ce7f04e36cb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 21:32:26 +0530

    feat: add HeaderMenu component to Header for improved navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
+import HeaderMenu from "./HeaderMenu";
 
 const Header = () => {
   return (
     <header className="bg-white py-5 ">
       <Container>
         <Logo />
+        <HeaderMenu/>
       </Container>
     </header>
   );
