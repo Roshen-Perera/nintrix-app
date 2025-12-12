250e63ea7fac05afa63050d254dcd3833f3aff02
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:06:41 +0530

    feat: enhance HeaderMenu styling for improved layout and readability

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const HeaderMenu = () => {
   return (
-    <div className="hidden md:inline-flex w-1/3">
+    <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor">
       {headerData?.map((item) => (
         <Link key={item?.title} href={item?.href}>
           {item?.title}
