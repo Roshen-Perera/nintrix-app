53ec88be74c95577c2449a672dd5794eacc4b221
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:29:41 +0530

    feat: add cursor pointer to Search icon in SearchBar component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SearchBar = () => {
   return <div>
-    <Search className="w-5 h-5 hover:text-shop_light_green hoverEffect"/>
+    <Search className="w-5 h-5 hover:text-shop_light_green hoverEffect cursor-pointer"/>
   </div>;
 };
 
