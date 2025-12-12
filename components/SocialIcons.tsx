3190b4e0c70ec8bceda5964ed96177f7876304d9
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-13 01:11:39 +0530

    feat: add social links and icons to SocialIcons component for enhanced social media integration

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -1,4 +1,28 @@
+import { Facebook, Github, Linkedin, Youtube } from 'lucide-react'
 import React from 'react'
+
+const socialLinks = [
+    {
+        name: "Facebook",
+        href: "https://www.facebook.com",
+        icon: <Facebook className="w-5 h-5" />,
+    },
+    {
+        name: "Github",
+        href: "https://www.github.com",
+        icon: <Github className="w-5 h-5" />,
+    },
+    {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com",
+        icon: <Linkedin className="w-5 h-5" />,
+    },
+    {
+        name: "YouTube",
+        href: "https://www.youtube.com",
+        icon: <Youtube className="w-5 h-5" />,
+    }
+]
 
 const SocialIcons = () => {
   return (
