eabff437a90085c86869d1bdf097a36c32e52abb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:40:48 +0530

    fix: update FavouriteButton link to point to favourites page

--- components/FavouriteButton.tsx
+++ components/FavouriteButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const FavouriteButton = () => {
   return (
-    <Link href={"/cart"} className="group relative">
+    <Link href={"/favourites"} className="group relative">
       <HeartIcon className="w-5 h-5 hover:text-shop_light_green hoverEffect" />
       <span className="absolute -top-1 -right-1 bg-shop_btn_dark_green text-white h-3.5 w-3.5 rounded-full flex items-center justify-center text-xs font-semibold">
         0
