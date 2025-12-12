51add0861f3cc789f8187ec3221627346427fe4f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 21:00:56 +0530

    feat: integrate Container component into Header for improved styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Container from './Container';
 
 const Header = () => {
   return (
     <header>
-        Header
+      <Container className='bg-red'>Header</Container>
     </header>
-  )
+  );
 }
 
 export default Header
