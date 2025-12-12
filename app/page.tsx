89a69afe4fcf11a092de1e8572ccece2e4b1cb9b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 20:52:57 +0530

    refactor: simplify Home component structure by removing unnecessary classes

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-10'>
+    <div>
       <h2 className='text-2xl font-semibold'>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ratione facilis quas, incidunt numquam sint a quo cumque voluptatibus in cupiditate possimus dignissimos vitae recusandae? Est animi nobis cum illum!</p>
-      <Button variant="default" className="mt-4">Default Button</Button>
+      <Button variant="default">Default Button</Button>
     </div>
   )
 }
