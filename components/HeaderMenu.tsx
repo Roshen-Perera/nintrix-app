740e15f37088af99f5542229d06422e9de1ec873
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:14:30 +0530

    feat: add decorative hover effects to HeaderMenu links for enhanced interactivity

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -8,6 +8,8 @@ const HeaderMenu = () => {
       {headerData?.map((item) => (
         <Link key={item?.title} href={item?.href} className={`hover:text-shop_light_green hoverEffect relative group`}>
           {item?.title}
+          <span className="absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0"/>
+          <span className="absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:right-0"/>
         </Link>
       ))}
     </div>
