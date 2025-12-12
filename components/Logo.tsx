f7985bb5f9b3704133f9749b2f55d0f721a2ce8a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 21:12:44 +0530

    fix: update Logo component to use Link for navigation

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Logo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <Link href={"/"}>
+    <h2>Shopcar<span>t</span></h2>
+  </Link>;
+};
 
-export default Logo
+export default Logo;
