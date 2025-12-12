ad85bc2e6ff8d6d236839e8f32eb024e96206c5f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 21:11:33 +0530

    fix: replace placeholder text with Logo component in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Container from "./Container";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <header className="bg-white py-5 ">
-      <Container>Header</Container>
+      <Container>
+        <Logo />
+      </Container>
     </header>
   );
 };
