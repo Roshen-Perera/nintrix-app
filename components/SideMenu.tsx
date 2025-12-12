57cb180015eeec149eee39e8fb0762894610e99d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-13 00:36:48 +0530

    fix: enhance SideMenu component with close button and text color adjustment

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import Logo from "./Logo";
+import { X } from "lucide-react";
 
 interface SideMenuProps {
   isOpen: boolean;
@@ -9,13 +10,16 @@ interface SideMenuProps {
 const SideMenu: FC<SideMenuProps> = ({ isOpen, onClose }) => {
   return (
     <div
-      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${
+      className={`fixed inset-y-0 h-screen left-0 z-50 w-full text-white/80 bg-black/50 shadow-xl ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       }hoverEffect`}
     >
       <div className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6">
         <div>
           <Logo className="text-white" />
+          <button className="hover:text-shop_light_green hoverEffect absolute top-6 right-6" onClick={onClose}>
+            <X />
+          </button>
         </div>
         <div></div>
       </div>
