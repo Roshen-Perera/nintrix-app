d95bbdad10dc117bcbc26b0ab595271d85ad9667
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-13 00:46:16 +0530

    fix: improve formatting and readability of SideMenu component

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -12,12 +12,15 @@ const SideMenu: FC<SideMenuProps> = ({ isOpen, onClose }) => {
     <div
       className={`fixed inset-y-0 h-screen left-0 z-50 w-full text-white/80 bg-black/50 shadow-xl ${
         isOpen ? "translate-x-0" : "-translate-x-full"
-      }hoverEffect`}
+      } hoverEffect`}
     >
       <div className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6">
         <div className="flex items-center justify-between gap-5">
           <Logo className="text-white" />
-          <button className="hover:text-shop_light_green hoverEffect absolute top-6 right-6" onClick={onClose}>
+          <button
+            className="hover:text-shop_light_green hoverEffect absolute top-6 right-6"
+            onClick={onClose}
+          >
             <X />
           </button>
         </div>
