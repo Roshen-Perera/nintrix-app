677961501f15e80d72e081102a43952222a59f7e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 20:59:32 +0530

    fix: update Container component to apply utility classes for styling

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,7 +1,8 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
-  return <div className={className}>{children}</div>;
+  return <div className={cn("max-w-7xl mx-auto px-4", className)}>{children}</div>;
 };
 
 export default Container;
