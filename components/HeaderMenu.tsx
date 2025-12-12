fb5931a171f11ff795979d5114c684998656130e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:07:48 +0530

    feat: add hover effects to HeaderMenu links for improved interactivity

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -6,7 +6,7 @@ const HeaderMenu = () => {
   return (
     <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor">
       {headerData?.map((item) => (
-        <Link key={item?.title} href={item?.href}>
+        <Link key={item?.title} href={item?.href} className={`hover:text-shop_light_green hoverEffect relative group`}>
           {item?.title}
         </Link>
       ))}
