a2b89096cb780040cc4e11d59da497bb8ebbac60
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-13 00:29:47 +0530

    fix: format SideMenu component for consistency and readability

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,16 +1,20 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
 
 interface SideMenuProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-const SideMenu:FC<SideMenuProps> = ({ isOpen, onClose }) => {
+const SideMenu: FC<SideMenuProps> = ({ isOpen, onClose }) => {
   return (
-    <div>
+    <div
+      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      }hoverEffect`}
+    >
       Side Menu
     </div>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
