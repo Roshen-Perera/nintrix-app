4941819365bdbe8bc3f705e650ee0a51963b4009
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:40:14 +0530

    feat: implement FavouriteButton component with link to cart

--- components/FavouriteButton.tsx
+++ components/FavouriteButton.tsx
@@ -1,7 +1,16 @@
+import { HeartIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const FavouriteButton = () => {
-  return <div>FavouriteButton</div>;
+  return (
+    <Link href={"/cart"} className="group relative">
+      <HeartIcon className="w-5 h-5 hover:text-shop_light_green hoverEffect" />
+      <span className="absolute -top-1 -right-1 bg-shop_btn_dark_green text-white h-3.5 w-3.5 rounded-full flex items-center justify-center text-xs font-semibold">
+        0
+      </span>
+    </Link>
+  );
 };
 
 export default FavouriteButton;
