1039d3c731e04c87bd65d67d2874220e89b9fb32
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:08:02 +0530

    feat: update HeaderMenu font weight for improved text appearance

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const HeaderMenu = () => {
   return (
-    <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor">
+    <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-normal text-lightColor">
       {headerData?.map((item) => (
         <Link key={item?.title} href={item?.href} className={`hover:text-shop_light_green hoverEffect relative group`}>
           {item?.title}
