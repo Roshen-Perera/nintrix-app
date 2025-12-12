324366689564402c7bdba30df99e8b93cc1db20e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-13 01:20:21 +0530

    feat: enhance SocialIcons component with props for customizable styling

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -1,33 +1,54 @@
-import { Facebook, Github, Linkedin, Youtube } from 'lucide-react'
-import React from 'react'
+import { Facebook, Github, Linkedin, Youtube } from "lucide-react";
+import React from "react";
+import { Tooltip, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
-const socialLinks = [
-    {
-        name: "Facebook",
-        href: "https://www.facebook.com",
-        icon: <Facebook className="w-5 h-5" />,
-    },
-    {
-        name: "Github",
-        href: "https://www.github.com",
-        icon: <Github className="w-5 h-5" />,
-    },
-    {
-        name: "LinkedIn",
-        href: "https://www.linkedin.com",
-        icon: <Linkedin className="w-5 h-5" />,
-    },
-    {
-        name: "YouTube",
-        href: "https://www.youtube.com",
-        icon: <Youtube className="w-5 h-5" />,
-    }
-]
-
-const SocialIcons = () => {
-  return (
-    <div>SocialIcons</div>
-  )
+interface Props {
+    className?: string;
+    iconClassName?: string;
+    toolTipClassName?: string;
 }
 
-export default SocialIcons
+const socialLinks = [
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com",
+    icon: <Facebook className="w-5 h-5" />,
+  },
+  {
+    name: "Github",
+    href: "https://www.github.com",
+    icon: <Github className="w-5 h-5" />,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com",
+    icon: <Linkedin className="w-5 h-5" />,
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com",
+    icon: <Youtube className="w-5 h-5" />,
+  },
+];
+
+const SocialIcons = ( { className, iconClassName, toolTipClassName }: Props ) => {
+  return (
+    <TooltipProvider>
+      <div className={cn("flex items-center gap-3.5", className)}>
+        {socialLinks?.map((item) => (
+          <Tooltip key={item?.name}>
+            <TooltipTrigger>
+              <Link key={item?.name} href={item?.href}>
+                {item?.icon}
+              </Link>
+            </TooltipTrigger>
+          </Tooltip>
+        ))}
+      </div>
+    </TooltipProvider>
+  );
+};
+
+export default SocialIcons;
