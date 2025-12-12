fbca122b919ddef50ccdda3c907e45190ee9b38e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-12 23:29:03 +0530

    feat: update SearchBar component to display search icon

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,10 @@
+import { Search } from "lucide-react";
 import React from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return <div>
+    <Search className="w-5 h-5 hover:text-shop_light_green hoverEffect"/>
+  </div>;
 };
 
 export default SearchBar;
