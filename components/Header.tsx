4ac8b283f9f4e354e4a6d75f0b8648cb1989210f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-13 00:05:38 +0530

    fix: add responsive classes to Header component for layout consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
       <Container className="flex items-center justify-between">
-        <div>
+        <div className="w-auto md:w-1/3 flex items-center gap-5">
           <Logo />
           <MobileMenu />
         </div>
