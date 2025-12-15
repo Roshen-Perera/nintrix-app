4103f59f2801da765e7f245028dc7f94412cf888
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-15 23:31:57 +0530

    feat: refactor Header component to directly fetch current user and remove HeaderWrapper

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
@@ -9,26 +7,25 @@ import FavouriteButton from "./FavouriteButton";
 import CartIcon from "./CardIcon";
 import SignInBtn from "./SignInBtn";
 import MobileMenu from "./MobileMenu";
+import { currentUser } from "@clerk/nextjs/server";
 import { ClerkLoaded, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/server";
 
-interface HeaderProps {
-  user: User | null;
-}
+const Header = async () => {
+  const user = await currentUser();
+  console.log("Current User:", user);
 
-const Header = ({ user }: HeaderProps) => {
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
       <Container className="flex items-center justify-between text-lightColor">
         <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
-          <MobileMenu />
+          <MobileMenu /> {/* Must have 'use client' */}
           <Logo />
         </div>
-        <HeaderMenu />
+        <HeaderMenu /> {/* Must have 'use client' */}
         <div className="w-auto md:1/3 flex items-center justify-end gap-5">
-          <SearchBar />
-          <CartIcon />
-          <FavouriteButton />
+          <SearchBar /> {/* Must have 'use client' */}
+          <CartIcon /> {/* Must have 'use client' */}
+          <FavouriteButton /> {/* Must have 'use client' */}
           <ClerkLoaded>
             <SignedIn>
               <UserButton />
