8edd15801acbebe1b30f747c9b1e89a49c11892c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:17:01 +0530

    fix: correct capitalization in promotional message of HomeBanner component

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -6,7 +6,7 @@ const HomeBanner = () => {
     <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
       <div>
         <Title>
-          Grab up to 50% off on <br />
+          Grab Upto 50% off on <br />
           selected items!
         </Title>
       </div>
