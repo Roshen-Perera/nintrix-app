27a5803773ea2ed62f59ca0d2ad2f6c692a7f873
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:14:44 +0530

    feat: add promotional message to HomeBanner component

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Title } from "./ui/text";
 
 const HomeBanner = () => {
   return (
     <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
-      <div></div>
+      <div>
+        <Title>
+          Grab up to 50% off on <br />
+          selected items!
+        </Title>
+      </div>
       <div></div>
     </div>
   );
