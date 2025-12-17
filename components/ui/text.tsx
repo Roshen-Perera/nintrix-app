9e31e01feb92a3ab93f638bc3de83fec1bd532ec
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:13:00 +0530

    fix: refactor Title component for improved readability and structure

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -1,9 +1,13 @@
-const Title = ({
+import { cn } from "@/lib/utils";
+
+export const Title = ({
   children,
   className,
 }: {
   children: React.ReactNode;
   className?: string;
 }) => {
-  return <h2 className={cn("text-2xl font-semibold", className)}>{children}</h2>;
+  return (
+    <h2 className={cn("text-2xl font-semibold", className)}>{children}</h2>
+  );
 };
