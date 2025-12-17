fb1812eaf906faad271271c8828ad6eae3e93542
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-17 23:51:00 +0530

    feat: add modal mode to SignInButton in SignInBtn component

--- components/SignInBtn.tsx
+++ components/SignInBtn.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SignInBtn = () => {
   return (
-    <SignInButton>
+    <SignInButton mode='modal'>
       <button className="text-sm font-semibold text-lightColor hover:cursor-pointer hover:text-darkColor hoverEffect">
         Login
       </button>
