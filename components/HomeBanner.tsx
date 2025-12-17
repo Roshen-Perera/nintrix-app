b08cb824de19b4c8283a5ecf72c069f61c1ea10f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:08:18 +0530

    fix: add padding and flex properties to HomeBanner component for improved layout

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeBanner = () => {
   return (
-    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg'>
+    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between'>
         HomeBanner
     </div>
   )
