55aa1e7ed9e933d626c903bcab4f92d7d2148590
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:09:53 +0530

    fix: standardize quotes and formatting in HomeBanner component

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const HomeBanner = () => {
   return (
-    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between'>
-        HomeBanner
+    <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
+      <div></div>
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
