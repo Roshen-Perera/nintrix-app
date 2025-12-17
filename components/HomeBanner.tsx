39d3f7ad784a0a8b1bb79b7772df42183493afb4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:24:12 +0530

    feat: add 'Buy Now' link to HomeBanner component

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Title } from "./ui/text";
+import Link from "next/link";
 
 const HomeBanner = () => {
   return (
@@ -9,6 +10,7 @@ const HomeBanner = () => {
           Grab Upto 50% off on <br />
           selected items!
         </Title>
+        <Link href={"/shop"} className="bg-shop_btn_dark_green font-semibold">Buy Now</Link>
       </div>
       <div></div>
     </div>
