bddb20e857f22081de7580154726a1ddb0f1d7a1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:07:04 +0530

    fix: add background color and border radius to HomeBanner component

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeBanner = () => {
   return (
-    <div className='py-16 md:py-0'>
+    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg'>
         HomeBanner
     </div>
   )
