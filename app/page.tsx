83100337fd9b2004612dd8f07c87af94e454c4f6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 00:00:20 +0530

    feat: replace placeholder content with HomeBanner component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Container from '@/components/Container'
-import { Button } from '@/components/ui/button'
+import HomeBanner from '@/components/HomeBanner'
 import React from 'react'
 
 const Home = () => {
   return (
     <Container>
-      <h2 className='text-2xl font-semibold'>Home</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ratione facilis quas, incidunt numquam sint a quo cumque voluptatibus in cupiditate possimus dignissimos vitae recusandae? Est animi nobis cum illum!</p>
-      <Button variant="default">Default Button</Button>
+      <HomeBanner />
     </Container>
   )
 }
