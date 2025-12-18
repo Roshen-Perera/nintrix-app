7f3cbb85a1827d61caaf913e0076cd8393b5b016
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 23:19:59 +0530

    style: format Footer component for improved readability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import Container from './Container'
-import FooterTop from './FooterTop'
-import Logo from './Logo'
-import SocialIcons from './SocialIcons'
+import React from "react";
+import Container from "./Container";
+import FooterTop from "./FooterTop";
+import Logo from "./Logo";
+import SocialIcons from "./SocialIcons";
 
 const Footer = () => {
   return (
-    <footer className='bg-white border-t'>
+    <footer className="bg-white border-t">
       <Container>
         <FooterTop />
         <div>
-          <div><Logo /><p>Discover the best products and deals with Shopcart, your one-stop online shop.</p><SocialIcons className='text-darkColor/60' iconClassName='border-darkColor/60 hover:border-shop_dark_green hover:text-shop_dark_green' toolTipClassName='bg-darkColor text-white'/></div>
+          <div>
+            <Logo />
+            <p>
+              Discover the best products and deals with Shopcart, your one-stop
+              online shop.
+            </p>
+            <SocialIcons
+              className="text-darkColor/60"
+              iconClassName="border-darkColor/60 hover:border-shop_dark_green hover:text-shop_dark_green hover:border-shop_dark_green"
+              toolTipClassName="bg-darkColor text-white"
+            />
+          </div>
           <div></div>
           <div></div>
           <div></div>
         </div>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
