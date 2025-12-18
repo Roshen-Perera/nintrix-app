3f6eea605466b76ccb0457b2c5b947182222ce90
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 23:22:07 +0530

    style: update layout of Footer component for improved responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <Container>
         <FooterTop />
-        <div>
+        <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="space-y-4">
             <Logo />
             <p>
