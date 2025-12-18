579c9cc512e2e3d60ac66a9052885806aae51263
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 23:18:03 +0530

    feat: add SocialIcons component to Footer for enhanced social media presence

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from './Container'
 import FooterTop from './FooterTop'
 import Logo from './Logo'
+import SocialIcons from './SocialIcons'
 
 const Footer = () => {
   return (
@@ -9,7 +10,7 @@ const Footer = () => {
       <Container>
         <FooterTop />
         <div>
-          <div><Logo /><p>Discover the best products and deals with Shopcart, your one-stop online shop.</p></div>
+          <div><Logo /><p>Discover the best products and deals with Shopcart, your one-stop online shop.</p><SocialIcons className='text-darkColor/60' iconClassName='border-darkColor/60 hover:border-shop_dark_green hover:text-shop_dark_green' toolTipClassName='bg-darkColor text-white'/></div>
           <div></div>
           <div></div>
           <div></div>
