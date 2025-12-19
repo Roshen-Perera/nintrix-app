3f010f0eb24d41e8eec93b65295368860b24de9b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-19 20:13:35 +0530

    style: replace paragraph with SubText component for improved accessibility

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,11 +12,11 @@ const Footer = () => {
         <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="space-y-4">
             <Logo />
-            <p className="text-gray-600 text-sm">
+            <SubText className="text-gray-600 text-sm">
               Find essentials and unique items with real reviews and clear
               pricing—plus fast delivery, secure checkout, easy returns, and
               responsive support.
-            </p>
+            </SubText>
             <SocialIcons
               className="text-darkColor/60"
               iconClassName="border-darkColor/60 hover:border-shop_dark_green hover:text-shop_dark_green hover:border-shop_dark_green"
