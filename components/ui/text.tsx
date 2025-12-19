9bc1fd1e701d6dbb6c46cebbe4afd2a3074e63bb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-19 20:10:22 +0530

    style: refactor Title component for improved readability and structure

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -8,6 +8,23 @@ export const Title = ({
   className?: string;
 }) => {
   return (
-    <h2 className={cn("text-3xl md:text-3xl font-bold text-shop_dark_green capitalize tracking-wide font-sans", className)}>{children}</h2>
+    <h2
+      className={cn(
+        "text-3xl md:text-3xl font-bold text-shop_dark_green capitalize tracking-wide font-sans",
+        className
+      )}
+    >
+      {children}
+    </h2>
   );
+};
+
+const subText = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <p>{children}</p>;
 };
