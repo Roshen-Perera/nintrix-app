87ae0cb7bb8195a6c98faf7d91063e1bec9d954f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-23 23:39:41 +0530

    feat: reintroduce Blog, Home, Shop, and RootLayout components with updated structure

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs"; 
