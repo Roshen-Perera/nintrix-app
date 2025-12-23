dd310ebd69877cee06112643bed11da808602bbd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-23 23:45:24 +0530

    refactor: enhance RootLayout by adding font and antialiasing styles to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-        <body>{children}</body>
+      <body className="font-poppins antialiased">{children}</body>
     </html>
   );
-}
-
+};
