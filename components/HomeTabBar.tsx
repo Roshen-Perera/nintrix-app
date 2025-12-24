4a5468e663ded762505e0dee897ff25762a5115d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 20:30:55 +0530

    fix: update button class for HomeTabBar to improve styling and ensure category selection works correctly

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -14,7 +14,8 @@ const HomeTabBar = ({ selectedCategory, onCategorySelect }: Props) => {
         {productType?.map((item) => (
           <button
             key={item?.title}
-            className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect`}
+            className={`border border-shop_light_green/20 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedCategory === item?.title ? 'bg-shop_light_green text-white border-shop_light_green' : 'bg-shop_light_green/20'}`}
+            onClick={() => onCategorySelect && onCategorySelect(item?.title)}
           >
             {item?.title}
           </button>
