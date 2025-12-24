037a4d09244ab77441ef89dfb3ec326e075b3218
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 20:15:44 +0530

    fix: remove unused layout and buttons in ProductGrid component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -11,14 +11,7 @@ const ProductGrid = () => {
   const [selectedCategory, setSelectedCategory] = useState(productType[0]?.title);
 
   return (
-    <div className="flex items-center justify-between flex-wrap gap-5">
-      <div className="flex items-center gap-3 text-sm font-semibold">
-        {productType?.map((item) => (
-          <button key={item?.title} className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect`}>{item?.title}</button>
-        ))}
-      </div>
-      <Link className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect`} href="/shop">See all</Link>
-    </div>
+    <div></div>
   );
 };
 
