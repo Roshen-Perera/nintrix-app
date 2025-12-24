62502183f65328f8553e941a8d52ad9af908eefb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 15:43:16 +0530

    feat: add selectedCategory state to ProductGrid component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 const ProductGrid = () => {
     const [products, setProducts] =  useState([]);
     const [loading, setLoading] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("");
 
   return <div>ProductGrid</div>;
 };
