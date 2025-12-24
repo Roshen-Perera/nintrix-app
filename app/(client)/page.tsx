16d05b62b5daf4d57aa74f5227370c784bf821d4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 15:33:59 +0530

    feat: add ProductGrid component to Home page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,11 +1,13 @@
 import Container from '@/components/Container'
 import HomeBanner from '@/components/HomeBanner'
+import ProductGrid from '@/components/ProductGrid'
 import React from 'react'
 
 const Home = () => {
   return (
     <Container className='bg-shop-light-pink'>
       <HomeBanner />
+      <ProductGrid />
     </Container>
   )
 }
