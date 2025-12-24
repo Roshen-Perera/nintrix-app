75dcc1b719fb57ca56010a9800baa17a0c6f31b6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 20:31:04 +0530

    style: add missing "use client" directive and standardize string quotes in HomeTabBar component

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { productType } from "@/constants/data";
 import Link from "next/link";
 import React from "react";
@@ -14,7 +16,7 @@ const HomeTabBar = ({ selectedCategory, onCategorySelect }: Props) => {
         {productType?.map((item) => (
           <button
             key={item?.title}
-            className={`border border-shop_light_green/20 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedCategory === item?.title ? 'bg-shop_light_green text-white border-shop_light_green' : 'bg-shop_light_green/20'}`}
+            className={`border border-shop_light_green/20 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedCategory === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/20"}`}
             onClick={() => onCategorySelect && onCategorySelect(item?.title)}
           >
             {item?.title}
