07cfe75dfaf748ac322803bc0a8fc2f8c0c17990
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 15:57:01 +0530

    fix: format code and improve return structure in ProductGrid component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -2,15 +2,19 @@
 
 import React, { useState } from "react";
 import HomeTabBar from "./HomeTabBar";
+import Link from "next/link";
 
 const ProductGrid = () => {
-    const [products, setProducts] =  useState([]);
-    const [loading, setLoading] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState("");
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-  return <div>
-    <HomeTabBar />
-  </div>;
+  return (
+    <div>
+      <div>left</div>
+      <Link href="/shop">See all</Link>
+    </div>
+  );
 };
 
 export default ProductGrid;
