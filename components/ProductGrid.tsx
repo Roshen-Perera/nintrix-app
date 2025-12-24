6bd6241900849394c48afbee30516baa6f0702b7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 20:24:09 +0530

    fix: ensure selectedCategory initializes with an empty string if no title is available

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -8,7 +8,7 @@ import { productType } from "@/constants/data";
 const ProductGrid = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState(productType[0]?.title);
+  const [selectedCategory, setSelectedCategory] = useState(productType[0]?.title || "");
 
   return (
     <div></div>
