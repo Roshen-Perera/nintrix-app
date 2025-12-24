e35116d6ffd204404df4b84d2a75a00541e9255b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-24 20:37:56 +0530

    fix: remove unnecessary console.log for selectedCategory in HomeTabBar component

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -10,6 +10,7 @@ interface Props {
 }
 
 const HomeTabBar = ({ selectedCategory, onCategorySelect }: Props) => {
+  console.log(selectedCategory);
   return (
     <div className="flex items-center justify-between flex-wrap gap-5">
       <div className="flex items-center gap-3 text-sm font-semibold">
