13742d02c95d4e01724efb31fc45dcdf2c9a7431
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-27 21:00:26 +0530

    feat: implement product fetching logic in ProductGrid component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 import Link from "next/link";
 import { productType } from "@/constants/data";
+import { set } from "sanity";
+import { client } from "@/sanity/lib/client";
 
 const ProductGrid = () => {
   const [products, setProducts] = useState([]);
@@ -12,9 +14,32 @@ const ProductGrid = () => {
     productType[0]?.title || ""
   );
 
+  const query = `*[_type == "product" && variant == "$variant"] | order(name desc){
+ ..., "categories":categories[]->title
+}`;
+const params = {variant: selectedCategory.toLowerCase()};
+
+useEffect(() => {
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await client.fetch(query, params);
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+}, [selectedCategory]);
+
+
   return (
     <div>
-      <HomeTabBar selectedCategory={selectedCategory} onCategorySelect={setSelectedCategory} />
+      <HomeTabBar
+        selectedCategory={selectedCategory}
+        onCategorySelect={setSelectedCategory}
+      />
     </div>
   );
 };
