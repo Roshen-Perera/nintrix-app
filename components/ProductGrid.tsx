a6a3876eba5ea8b8604553502064e051748427fc
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-03 13:55:15 +0530

    fix: replace motion div with static loading UI in ProductGrid component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -44,10 +44,10 @@ const ProductGrid = () => {
       />
       {!loading ? (
         <div className="flex flex-col justify-center items-center py-10 min-h-80 gap-4  bg-gray-100 w-full mt-10">
-          <motion.div>
-            <Loader2 />
+          <div className="space-x-2 flex items-center text-blue-600">
+            <Loader2 className="w-5 h-6 animate-spin"/>
             <span>Products are loading...</span>
-          </motion.div>
+          </div>
         </div>
       ) : (
         <>products</>
