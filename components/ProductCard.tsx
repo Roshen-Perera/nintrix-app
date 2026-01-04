b2f9dc0ee8c6a6205c1c66b8b34ca0479147f327
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 23:33:08 +0530

    fix: remove unused Title import in ProductCard component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import AddToWishlistBtn from "./AddToWishlistBtn";
-import { Title } from "./ui/text";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
