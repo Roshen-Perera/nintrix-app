6ade828d068ed8097a65a9e3a40b15ee00c8f9c0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-05 00:22:11 +0530

    feat: update AddToCartBtn layout and improve stock status display

--- components/AddToCartBtn.tsx
+++ components/AddToCartBtn.tsx
@@ -1,17 +1,22 @@
-import { Product } from '@/sanity.types';
-import React from 'react'
+import { Product } from "@/sanity.types";
+import React from "react";
+import { Button } from "./ui/button";
+import { ShoppingBagIcon } from "lucide-react";
 
 interface Props {
-    product?: Product;
-    className?: string;
+  product?: Product;
+  className?: string;
 }
 
 const AddToCartBtn = ({ product, className }: Props) => {
+    const isOutOfStock = product?.stock === 0;
   return (
     <div>
-      addcart
+      <Button>
+        <ShoppingBagIcon /> {isOutOfStock ? "Out of Stock" : "Add to Cart"}
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default AddToCartBtn
+export default AddToCartBtn;
