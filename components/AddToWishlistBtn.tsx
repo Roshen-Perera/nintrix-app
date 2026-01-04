d792e8d670e4f31bce4a451f1a0f59de79c6f0fc
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 22:02:15 +0530

    fix: refactor AddToWishlistBtn to accept product and className props

--- components/AddToWishlistBtn.tsx
+++ components/AddToWishlistBtn.tsx
@@ -1,11 +1,22 @@
+import { cn } from '@/lib/utils';
+import { Product } from '@/sanity.types';
+import { Heart } from 'lucide-react';
 import React from 'react'
 
-const AddToWishlistBtn = () => {
+const AddToWishlistBtn = ({
+    product,
+    className
+}: {
+    product: Product;
+    className?: string;
+}) => {
   return (
-    <div>
-      AWB
+    <div className={cn("absolute top-2 right-2 z-10", className)}>
+      <div className={`p-2.5 rounded-full hover:bg-shop_btn_dark_green hover:text-white hoverEffect bg-white`}>
+        <Heart size={15} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default AddToWishlistBtn
