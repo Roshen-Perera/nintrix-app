05f54482d5edf5afcd1e07d34040261512e59110
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 22:07:20 +0530

    fix: format code in AddToWishlistBtn for consistency

--- components/AddToWishlistBtn.tsx
+++ components/AddToWishlistBtn.tsx
@@ -1,22 +1,24 @@
-import { cn } from '@/lib/utils';
-import { Product } from '@/sanity.types';
-import { Heart } from 'lucide-react';
-import React from 'react'
+import { cn } from "@/lib/utils";
+import { Product } from "@/sanity.types";
+import { Heart } from "lucide-react";
+import React from "react";
 
 const AddToWishlistBtn = ({
-    product,
-    className
+  product,
+  className,
 }: {
-    product: Product;
-    className?: string;
+  product: Product;
+  className?: string;
 }) => {
   return (
     <div className={cn("absolute top-2 right-2 z-10", className)}>
-      <div className={`p-2.5 rounded-full hover:bg-shop_btn_dark_green hover:text-white hoverEffect bg-shop_lighter_bg`}>
+      <div
+        className={`p-2.5 rounded-full hover:bg-shop_btn_dark_green hover:text-white hoverEffect bg-shop_lighter_bg`}
+      >
         <Heart size={15} />
       </div>
     </div>
   );
-}
+};
 
-export default AddToWishlistBtn
+export default AddToWishlistBtn;
