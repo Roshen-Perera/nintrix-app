85931dea5e46bbacfb3b80e2c02ef23c8b639226
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 21:31:03 +0530

    fix: add hot status link and flame icon to ProductCard component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,8 @@
 import { Product } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
+import { Flame } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ProductCard = ({ product }: { product: Product }) => {
@@ -20,6 +22,18 @@ const ProductCard = ({ product }: { product: Product }) => {
           <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green group-hover:text-shop_light_green hoverEffect">
             Sale!
           </p>
+        )}
+        {product?.status === "hot" && (
+          <Link
+            href={"/deal"}
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect"
+          >
+            <Flame
+              size={18}
+              fill="#fb6c08"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
         )}
       </div>
       <div className="p-3">Product detals</div>
