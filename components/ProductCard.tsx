9e492931ac4eff324681c82da9c0f9c6e4486558
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 21:02:37 +0530

    fix: add styling to ProductCard component for improved layout

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div>
+    <div className="text-sm border">
       <div>
         {product?.images && (
           <Image
