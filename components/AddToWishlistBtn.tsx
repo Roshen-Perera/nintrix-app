016816a6d1499c324b95b4a4b75ee9af5b48ab85
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 22:09:54 +0530

    fix: remove unused CSS variables from globals.css

--- components/AddToWishlistBtn.tsx
+++ components/AddToWishlistBtn.tsx
@@ -13,7 +13,7 @@ const AddToWishlistBtn = ({
   return (
     <div className={cn("absolute top-2 right-2 z-10", className)}>
       <div
-        className={`p-2.5 rounded-full hover:bg-shop_btn_dark_green hover:text-white hoverEffect bg-shop_lighter_bg`}
+        className={`p-1 rounded-full hover:bg-shop_btn_dark_green hover:text-white hoverEffect bg-shop_lighter_bg`}
       >
         <Heart size={15} />
       </div>
