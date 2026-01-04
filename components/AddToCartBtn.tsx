5fe9a78343ba1798a20016c6208be24eb575a017
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-05 00:24:20 +0530

    feat: refactor AddToCartBtn to improve button styling and layout

--- components/AddToCartBtn.tsx
+++ components/AddToCartBtn.tsx
@@ -2,6 +2,7 @@ import { Product } from "@/sanity.types";
 import React from "react";
 import { Button } from "./ui/button";
 import { ShoppingBagIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   product?: Product;
@@ -9,10 +10,15 @@ interface Props {
 }
 
 const AddToCartBtn = ({ product, className }: Props) => {
-    const isOutOfStock = product?.stock === 0;
+  const isOutOfStock = product?.stock === 0;
   return (
     <div>
-      <Button>
+      <Button
+        className={cn(
+          "w-full bg-shop_dark_green/80 text-lightBg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect",
+          className
+        )}
+      >
         <ShoppingBagIcon /> {isOutOfStock ? "Out of Stock" : "Add to Cart"}
       </Button>
     </div>
