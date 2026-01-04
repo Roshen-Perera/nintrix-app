1d12fadb9da8d36deb7e5f81212099ed02351899
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-04 23:54:46 +0530

    fix: add stock availability display in ProductCard component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -72,6 +72,10 @@ const ProductCard = ({ product }: { product: Product }) => {
             5 Reviews
           </p>
         </div>
+        <div className="flex items-center gap-2.5">
+          <p className="font-medium">In Stock</p>
+          <p className={`font-semibold ${product?.stock === 0 ? "text-red-600" : "text-shop_light_green"}`}>{(product?.stock as number) > 0 ? product?.stock : "Out of Stock"}</p>
+        </div>
       </div>
     </div>
   );
